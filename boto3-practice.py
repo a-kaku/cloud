4725a91949d76cad33d3626a19703793cd890fc3
akira <tooru_sann@yahoo.co.jp>
2025-12-07 15:59:28 +0900

    The first practice of boto3.

---
+++ boto3-practice.py
@@ -0,0 +1,11 @@
+import boto3
+
+s3 = boto3.client('s3')
+s3.upload_file(
+    Filename = 'boto3-practice.py',
+    Bucket = 'akira.bucket',
+    Key = '/cloud-script/boto3-practice.py'
+)
+
+buckets_list = s3.list_buckets()
+print(buckets_list)
