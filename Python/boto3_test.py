00c12034c46c8a4df34d5f5b4a86fe415c31f1d5
郭 明 <m-kaku@hgs.h21group.com>
2025-12-09 16:27:09 +0900

    Practice for boto3 library

---
+++ Python/boto3_test.py
@@ -0,0 +1,22 @@
+import boto3
+
+def stop_ec2(InstanceIds):
+    """"Stops an Instance."""
+
+    ec2 = boto3.client('ec2')
+    try:
+        response = ec2.stop_instances(InstanceIds=InstanceIds)
+    except Exception as e:
+        print(f"Reboot failed: {e}")
+
+def start_ec2(InstanceIds):
+    """Starts an instance."""
+    ec2 = boto3.client('ec2')
+    try:
+        response = ec2.start_instances(InstanceIds=InstanceIds)
+    except Exception as e:
+        print(f"Start instance failed: {e}")
+
+instance_ids = ['i-0da38292f63136e56']
+# stop_ec2(instance_ids)
+start_ec2(instance_ids)
