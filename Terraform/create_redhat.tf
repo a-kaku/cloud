05b3fdc7de0c4f6b19381315349ef880687e9360
郭 明 <m-kaku@hgs.h21group.com>
2025-12-19 14:00:56 +0900

    Create Rethat instance.

--- Terraform/create_redhat.tf
+++ Terraform/create_redhat.tf
@@ -1,4 +1,19 @@
 resource 'aws_instance' 'SFTP-tmp01' {
-    ami           = ''
+    ami           = 'ami-040573aabcd4f9b69'
+    instance_type = 't3.medium'
+    
 
+    tags          = {
+        Name = 'SFTP-tmp01'
+        Owner = 'h21local'
+        CmBillingGroup = 'h21local'
+    }
+}
+
+resource 'aws_vpc' 'vpc-h21group' {
+    vpc_id = 'vpc-66901d03'
+}
+
+resource 'aws_subnet' 'h21local-d' {
+    subnet_id = 'subnet-0ace5ac2c0711268e'
 }
