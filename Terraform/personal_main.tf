6968b12816a85cd8cc000ffb5737723980549785
akira <tooru_sann@yahoo.co.jp>
2025-12-20 15:09:19 +0900

    NEW

--- Terraform/personal_main.tf
+++ Terraform/personal_main.tf
@@ -3,10 +3,23 @@ provider "aws" {
 }
 
 resource "aws_instance" "personal" {
-    tags = {
-        Name = "PersonalServer001"
-    }
 
     instance_type = "t2.micro"
     ami = "ami-040573aabcd4f9b69"
+    vpc_security_group_ids = [aws_security_group.sg.id]
+
+    tags = {
+        Name = "PersonalServer001"
+    }
+}
+
+resource "aws_security_group" "sg" {
+    name = "launch-wizard-1"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
