1f556a0c79d95450b9625420ed416dd3501e9a24
郭 明 <m-kaku@hgs.h21group.com>
2025-12-22 17:45:13 +0900

    modified 12/22

--- Terraform/SFTP/redhat_main.tf
+++ Terraform/SFTP/redhat_main.tf
@@ -18,21 +18,4 @@ data "aws_iam_instance_profile" "ec2_for_efs_s3_cloudwatch" {
   name = "ec2_for_efs_s3_cloudwatch"
 }
 
-resource "aws_instance" "sftp_tmp01" {
-  ami           = "ami-040573aabcd4f9b69"
-  instance_type = "t3.medium"
-  subnet_id     = data.aws_subnet.h21local_d.id
 
-  vpc_security_group_ids = [
-    data.aws_security_group.sftp_test.id
-  ]
-
-  iam_instance_profile = data.aws_iam_instance_profile.ec2_for_efs_s3_cloudwatch.name
-
-  tags = {
-    Name            = "SFTP-tmp01"
-    Owner           = "h21local"
-    CmBillingGroup  = "h21local"
-    Role           = "sftp-server"
-  }
-}
