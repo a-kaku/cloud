bc2a848b031fe78429458db7e879fb4e9160ad67
郭 明 <m-kaku@hgs.h21group.com>
2025-12-24 10:01:12 +0900

    new

--- Terraform/Personal/s3.tf
+++ Terraform/Personal/s3.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "akira-bucket" {
     bucket = "akira.bucket"
 }
 
-resource "aws_dynamodb" "terraform_locks" {
+resource "aws_dynamodb_table" "terraform_locks" {
   name = "terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "LockID"
