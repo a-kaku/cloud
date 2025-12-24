f2cd29a117a0ecb6ca40a2ceda89c522e7a6a813
郭 明 <m-kaku@hgs.h21group.com>
2025-12-24 13:57:18 +0900

    new

--- Terraform/SFTP/efs.tf
+++ Terraform/SFTP/efs.tf
@@ -1,4 +1,14 @@
 resource "aws_efs_file_system" "sftp_efs" {
+    
+    throughput_mode = "elastic"
+    lifecycle_policy {
+      transition_to_ia = "AFTER_30_DAYS"
+    }
+
+    lifecycle_policy {
+      transition_to_archive = "AFTER_90_DAYS"
+    }
+    
     tags = {
       Name           = "SFTP-Shared"
       CmBillingGroup = "h21local"
