f84451f02151ddbd96de7e22dbe194beda11020b
郭 明 <m-kaku@hgs.h21group.com>
2025-12-24 13:09:18 +0900

    modified:12/24

--- Terraform/SFTP/nlb.tf
+++ Terraform/SFTP/nlb.tf
@@ -22,8 +22,8 @@ resource "aws_lb_listener" "hgs_nlb_listener" {
   }
 }
 
-resource "aws_lb_target_group" "hgs_nlb_tg" {
-  name     = "hgs-nlb-tg"
+resource "aws_lb_target_group" "hgs_nlb_tg01" {
+  name     = "hgs-nlb-tg01"
   port     = 22
   protocol = "TCP"
   target_type = "instance"
@@ -40,6 +40,23 @@ resource "aws_lb_target_group_attachment" "sftp_tmp01_attachment" {
   port             = 22
 }
 
+resource "aws_lb_target_group" "hgs_nlb_tg02" {
+  name     = "hgs-nlb-tg02"
+  port     = 22
+  protocol = "TCP"
+  target_type = "instance"
+  vpc_id = "vpc-66901d03"
+
+  tags = {
+    Name = "hgs-nlb-tg"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "sftp_tmp02_attachment" {
+  target_group_arn = aws_lb_target_group.hgs_nlb_tg.arn
+  target_id        = aws_instance.sftp_tmp02.id
+  port             = 22
+}
 
 data "aws_security_group" "sftp_test" {
   name        = "SFTP-test"
