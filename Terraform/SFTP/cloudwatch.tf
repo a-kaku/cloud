4b37880640782d14d90993766b5a7ef2dc5f628d
郭 明 <m-kaku@hgs.h21group.com>
2025-12-25 15:18:28 +0900

    new

--- Terraform/SFTP/cloudwatch.tf
+++ Terraform/SFTP/cloudwatch.tf
@@ -1,14 +1,14 @@
 resource "aws_cloudwatch_log_group" "sftp" {
-    name = "/aws/instance/sftp/log/sftp.log"
+    name = "/aws/instance/sftp/log/${aws_instance.sftp.id}-sftp.log"
     retention_in_days = 30
 }
 
-resource "aws_cloud_watch_log_group" "sftp_process" {
-    name = "/aws/instance/sftp/log/sftp_process.log"
+resource "aws_cloudwatch_log_group" "sftp_process" {
+    name = "/aws/instance/sftp/log/${aws_instance.sftp.id}-sftp_process.log"
     retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "secure" {
-    name = "/aws/instance/sftp/log/secure.log"
+    name = "/aws/instance/sftp/log/${aws_instance.sftp.id}-secure.log"
     retention_in_days = 30
 }
