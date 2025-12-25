5007e19c9158c94bfb227f8a5e29aa7a71c61f24
郭 明 <m-kaku@hgs.h21group.com>
2025-12-25 15:38:29 +0900

    new

--- Terraform/SFTP/cloudwatch.tf
+++ Terraform/SFTP/cloudwatch.tf
@@ -1,29 +1,29 @@
 resource "aws_cloudwatch_log_group" "sftp_01" {
-    name = "/aws/instance/sftp/log/${aws_instance.sftp_tmp01.id}-sftp.log"
+    name = "/aws/instance/sftp/log/${aws_instance.sftp_01.id}-sftp.log"
     retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "sftp_process_01" {
-    name = "/aws/instance/sftp/log/${aws_instance.sftp_tmp01.id}-sftp_process.log"
+    name = "/aws/instance/sftp/log/${aws_instance.sftp_01.id}-sftp_process.log"
     retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "secure_01" {
-    name = "/aws/instance/sftp/log/${aws_instance.sftp_tmp01.id}-secure.log"
+    name = "/aws/instance/sftp/log/${aws_instance.sftp_01.id}-secure.log"
     retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "sftp_02" {
-    name = "/aws/instance/sftp/log/${aws_instance.sftp_tmp02.id}-sftp.log"
+    name = "/aws/instance/sftp/log/${aws_instance.sftp_02.id}-sftp.log"
     retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "sftp_process_02" {
-    name = "/aws/instance/sftp/log/${aws_instance.sftp_tmp02.id}-sftp_process.log"
+    name = "/aws/instance/sftp/log/${aws_instance.sftp_02.id}-sftp_process.log"
     retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "secure_02" {
-    name = "/aws/instance/sftp/log/${aws_instance.sftp_tmp02.id}-secure.log"
+    name = "/aws/instance/sftp/log/${aws_instance.sftp_02.id}-secure.log"
     retention_in_days = 30
 }
