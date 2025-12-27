bc8a5f3361b895215a3a0ee11debab5b73b637f3
akira <tooru_sann@yahoo.co.jp>
2025-12-27 16:17:02 +0900

    data

---
+++ Terraform/Personal/data-stores/mysql/MySQL.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "mysql" {
+    engine = "mysql"
+    allocated_storage = 10
+    instance_class = "db.t2.micro"
+    db_name = "personal_db"
+    skip_final_snapshot = true
+    identifier_prefix = "terraform-up-and-running"
+
+    username = var.db_username
+    password = var.db_password
+}
