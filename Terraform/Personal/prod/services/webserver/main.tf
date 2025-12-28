35363ba63ba1887bb030cf8c5890d03d938f5b1f
akira <tooru_sann@yahoo.co.jp>
2025-12-28 17:22:52 +0900

    new

--- Terraform/Personal/prod/services/webserver/main.tf
+++ Terraform/Personal/prod/services/webserver/main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 module "webserver" {
   source = "../../../modules/services/webserver"
   server_name = "module_test_web"
-  db_remote_state_bucket = "/personal-practice-bucket/prod/services/webserver/" 
-  db_remote_state_key = "terraform-locks"
+  db_remote_state_bucket = "personal-practice-bucket" 
+  db_remote_state_key = "prod/services/webserver/terraform.tfstate"
 }
