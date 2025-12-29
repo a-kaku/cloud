5bed1a29f78b7e7c0dc4d09d121d58f163fd7e81
akira <tooru_sann@yahoo.co.jp>
2025-12-29 14:22:19 +0900

    new

--- Terraform/Personal/prod/services/webserver/terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        region = "ap-northeast-1"
-        bucket = "personal-practice-bucket"
-        key = "prod/services/webserver/terraform.tfstate"
-        dynamodb_table = "terraform-locks"
-        encrypt = true
-    }
-}
