9c6415beebc868c9e8c9405582db0f7239988140
akira <tooru_sann@yahoo.co.jp>
2025-12-29 19:20:24 +0900

    new

--- Terraform/Personal/prod/services/webserver/main.tf
+++ Terraform/Personal/prod/services/webserver/main.tf
@@ -1,6 +1,7 @@
 module "module_server" {
     source = "../../../modules/services/webserver"
-    server_name = "module_server_1"
+    count = 2
+    server_name = "module_server_${count.index}"
     db_remote_state_bucket = "personal_practice_bucket"
     db_remote_state_key = "prod/services/webserver/terraform.tfstate"
 }
