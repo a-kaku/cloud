2061d096bb231f4acd238f074bf029cf3a0d2722
akira <tooru_sann@yahoo.co.jp>
2025-12-31 16:34:33 +0900

    new

--- Terraform/Personal/prod/services/webserver/main.tf
+++ Terraform/Personal/prod/services/webserver/main.tf
@@ -11,13 +11,13 @@ module "module_server" {
             from_port = 22
             to_port = 22
             protocol = "tcp"
-            cidr_blocks = "0.0.0.0/0"
+            cidr_blocks = ["0.0.0.0/0"]
         },
         {
             from_port = 443
             to_port = 443
             protocol = "tcp"
-            cidr_blocks = "0.0.0.0/0"
+            cidr_blocks = ["0.0.0.0/0"]
         }
     ]
 
@@ -26,7 +26,7 @@ module "module_server" {
             from_port = 0
             to_port = 0
             protocol = -1
-            cidr_blocks = "0.0.0.0/0"
+            cidr_blocks = ["0.0.0.0/0"]
         }
     ]
 
