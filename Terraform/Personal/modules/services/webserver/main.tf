60cedb483ca3ceaa7268d50283c1edd3d2e0dfcf
akira <tooru_sann@yahoo.co.jp>
2025-12-31 16:53:07 +0900

    new

--- Terraform/Personal/modules/services/webserver/main.tf
+++ Terraform/Personal/modules/services/webserver/main.tf
@@ -19,20 +19,20 @@ resource "aws_security_group" "sg" {
     dynamic "ingress" {
         for_each = var.ingress_rule
         content {
-          from_port = ingress.from_port
-          to_port = ingress.to_port
-          protocol = ingress.protocol
-          cidr_blocks = ingress.cidr_blocks
+          from_port = ingress.value.from_port
+          to_port = ingress.value.to_port
+          protocol = ingress.value.protocol
+          cidr_blocks = ingress.value.cidr_blocks
         }
     }
 
     dynamic "egress" {
         for_each = var.egress_rule
         content {
-          from_port = egress.from_port
-          to_port = egress.to_port
-          protocol = egress.protocol
-          cidr_blocks = egress.cidr_blocks
+          from_port = egress.value.from_port
+          to_port = egress.value.to_port
+          protocol = egress.value.protocol
+          cidr_blocks = egress.value.cidr_blocks
         }
     }
 }
