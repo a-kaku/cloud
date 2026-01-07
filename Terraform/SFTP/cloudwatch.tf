98a620e86f831a8b36e69f528405a5870922308c
郭 明 <m-kaku@hgs.h21group.com>
2026-01-07 13:52:16 +0900

    new

--- Terraform/SFTP/cloudwatch.tf
+++ Terraform/SFTP/cloudwatch.tf
@@ -1,17 +1,17 @@
 resource "aws_cloudwatch_log_group" "sftp" {
-    for_each = module.sftp.instance_ids
+    for_each = var.instances
     name = "/aws/instance/sftp/log/${each.value}-sftp.log"
     retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "sftp_process" {
-    for_each = module.sftp.instance_ids
+    for_each = var.instances
     name = "/aws/instance/sftp/log/${each.value}-sftp_process.log"
     retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "secure" {
-    for_each = module.sftp.instance_ids
+    for_each = var.instances
     name = "/aws/instance/sftp/log/${each.value}-secure.log"
     retention_in_days = 30
 }
