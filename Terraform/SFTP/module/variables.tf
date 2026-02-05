d179f18401ac2995d4d1a1deb904474a477898b9
Ethan <tooru_sann@yahoo.co.jp>
2026-02-05 16:19:25 +0900

    new

--- Terraform/SFTP/module/variables.tf
+++ Terraform/SFTP/module/variables.tf
@@ -65,14 +65,14 @@ variable "egress_rule" {
 }
 */
 
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket to be created."
-  type = string
-  default = "h21.sftp.backup"
-}
+# variable "s3_bucket_name" {
+#   description = "The name of the S3 bucket to be created."
+#   type = string
+#   default = "h21.sftp.backup"
+# }
 
-variable "s3_bucket_expiration_days" {
-  description = " Number of days after which objects in the S3 bucket will expire."
-  type = number
-  default = 30
-}
+# variable "s3_bucket_expiration_days" {
+#   description = " Number of days after which objects in the S3 bucket will expire."
+#   type = number
+#   default = 30
+# }
