043792916d4eb5960ed2c77326420022fec3df76
Ethan <tooru_sann@yahoo.co.jp>
2026-02-06 15:15:52 +0900

    new

--- Terraform/SFTP/module/ec2/output.tf
+++ Terraform/SFTP/module/ec2/output.tf
@@ -1,7 +1,7 @@
 output "instance_ids" {
   description = "The IDs of the EC2 instances."
   value       = {
-    for k, v in aws_instance.sftp :
+    for k, v in aws_instance.sftp_instance :
         k => v.id
   }
 }
