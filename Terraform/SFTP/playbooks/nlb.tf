070136a39117e3e636e791644389edcd9003eb53
Ethan <tooru_sann@yahoo.co.jp>
2026-02-06 16:49:38 +0900

    new

--- Terraform/SFTP/playbooks/nlb.tf
+++ Terraform/SFTP/playbooks/nlb.tf
@@ -1,5 +1,5 @@
 module "new_nlb" {
     source = "../module/nlb"
-    eip_allocation_ids = module.new_nlb.eip_allocation_ids
+    eip_allocation_ids = module.subnet_eip.eip_allocation_ids
     target_instance_ids = module.sftp_instance.instance_ids
 }
