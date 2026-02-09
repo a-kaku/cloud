edd050764b831fd54b6899d3e43c74a06d809646
Ethan <tooru_sann@yahoo.co.jp>
2026-02-09 17:54:15 +0900

    new

--- Python/boto3_test.py
+++ Python/boto3_test.py
@@ -3,6 +3,7 @@ import boto3
 def stop_ec2(InstanceIds):
     """"Stops an Instance."""
 
+    aws_region = 'ap-northeast-1'
     ec2 = boto3.client('ec2', region_name=aws_region)
     try:
         response = ec2.stop_instances(InstanceIds=InstanceIds)
@@ -17,7 +18,6 @@ def start_ec2(InstanceIds):
     except Exception as e:
         print(f"Start instance failed: {e}")
 
-aws_region = 'ap-northeast-1'
 instance_ids = ['i-0da38292f63136e56']
 # stop_ec2(instance_ids)
 start_ec2(instance_ids)
