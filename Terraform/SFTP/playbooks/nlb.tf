8839f046fa0e3450d5b174b810ab744e24ed7f43
Ethan <tooru_sann@yahoo.co.jp>
2026-02-12 17:16:55 +0900

    new

--- Terraform/SFTP/playbooks/nlb.tf
+++ Terraform/SFTP/playbooks/nlb.tf
@@ -2,7 +2,7 @@ module "new_nlb" {
     source = "../module/nlb"
     eip_allocation_ids = module.eip.eip_allocation_ids
     target_instance_ids = {
-      sftp01 = aws_instance.sftp["sftp-01"].id
-      sftp02 = aws_instance.sftp["sftp-02"].id
+      sftp01 = aws_instance.sftp_instance["sftp-01"].id
+      sftp02 = aws_instance.sftp_instance["sftp-02"].id
     }
 }
