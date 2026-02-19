e82b57a9091c71823accc869dc84b77907ce40d0
Ethan <tooru_sann@yahoo.co.jp>
2026-02-19 15:00:53 +0900

    new

--- Terraform/SFTP/module/security_group/main.tf
+++ Terraform/SFTP/module/security_group/main.tf
@@ -26,3 +26,12 @@ resource "aws_security_group_rule" "ingress" {
     description = lookup(each.value, "description", null)
 
 }
+
+resource "aws_security_group_rule" "egress" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    security_group_id = aws_security_group.sg.id
+    cidr_blocks = ["0.0.0.0/0"]
+}
