ef8904faf5fda58f4f4f7bfc5f63cb6b58c1a7d0
Ethan <tooru_sann@yahoo.co.jp>
2026-02-19 14:28:43 +0900

    new

--- Terraform/SFTP/module/security_group/main.tf
+++ Terraform/SFTP/module/security_group/main.tf
@@ -12,7 +12,10 @@ resource "aws_security_group" "sg" {
 
 resource "aws_security_group_rule" "ingress" {
 
-    for_each = toset(var.ingress_rules)
+    for_each = {
+        for idx, rule in var.ingress_rules :
+        idx => rule
+    }
     type = "ingress"
     from_port = each.value.from_port
     to_port = each.value.to_port
