57b144deb527379379f6acc39f05aa86eac19865
Ethan <tooru_sann@yahoo.co.jp>
2026-02-19 14:17:28 +0900

    new

---
+++ Terraform/SFTP/module/security_group/main.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "sg" {
+    description = "Allow inbound traffic and all outbound traffic"
+    name = var.sg_name
+    vpc_id = var.vpc_id
+
+    tags = {
+        Name = var.sg_name
+        CmBillingGroup = var.cm_billing_group
+    }
+
+}
+
+resource "aws_security_group_rule" "ingress" {
+
+    for_each = toset(var.ingress_rules)
+    type = "ingress"
+    from_port = each.value.from_port
+    to_port = each.value.to_port
+    protocol = each.value.protocol
+    security_group_id = aws_security_group.sg.id
+    
+}
