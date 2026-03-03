337bbf68ba254728c258759d5d82639c71fdd83c
Ethan <tooru_sann@yahoo.co.jp>
2026-03-03 11:48:20 +0900

    new

--- Terraform/SFTP/playbooks/nlb.tf
+++ Terraform/SFTP/playbooks/nlb.tf
@@ -4,21 +4,3 @@ module "nlb" {
   eip_allocation_ids = var.eip_allocation_ids
   target_instance_ids = var.target_instance_ids
 }
-
-module "listener" {
-  source = "../module/listener"
-
-  target_instance_ids = var.target_instance_ids
-}
-
-module "tg" {
-  source = "../module/target_group"
-
-  target_instance_ids = var.target_instance_ids
-}
-
-module "attachment" {
-  source = "../module/attachment"
-
-  target_instance_ids = var.target_instance_ids
-}
