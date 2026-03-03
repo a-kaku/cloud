84083b2df43a96695282cb03e4a3a4e0f43965be
Ethan <tooru_sann@yahoo.co.jp>
2026-03-03 15:55:58 +0900

    Route53

---
+++ Terraform/SFTP/module/route53/main.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.hosted_zone
+  name    = var.domain_name
+  type    = var.type
+  ttl     = 300
+  records = [var.record]
+}
+
+data "aws_route53_zone" "hosted_zone" {
+    name = var.hosted_zone_name
+}
