e82186553175b9591354cd10ae9a6b4f3bfc073c
Ethan <tooru_sann@yahoo.co.jp>
2026-03-03 16:04:37 +0900

    new

--- Terraform/SFTP/module/route53/main.tf
+++ Terraform/SFTP/module/route53/main.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "www" {
-  zone_id = data.aws_route53_zone.hosted_zone
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = var.domain_name
   type    = var.type
   ttl     = 300
